5566e14d6b6819acf67cd88988a7eab5582f1769
aimielbn <144000676+aimielbn@users.noreply.github.com>
2023-10-03 11:19:19 +0200

    Personnalisation de Bootstrap avec Sass

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue';
 import App from './App.vue';
 
 import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/custom.scss';
 
 createApp(App).mount('#app');
