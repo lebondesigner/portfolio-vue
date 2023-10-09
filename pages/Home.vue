8a44ae0a96d80e823b99c7483c8c7bfe452e6c76
aimielbn <aimie.lebon@ifosup.wavre.be>
2023-10-09 11:45:32 +0200

    home

--- pages/Home.vue
+++ pages/Home.vue
@@ -5,12 +5,12 @@ import { RouterLink } from 'vue-router';
 <template>
   <div class="jumbotron">
     <div class="container">
-      <h1 class="display-4">Bienvenue sur Mon Site Web</h1>
+      <h1 class="display-4">Coucou</h1>
       <p class="lead">
         Votre source fiable d'informations et de ressources en ligne.
       </p>
       <RouterLink to="/about" class="btn btn-primary btn-lg"
-        >En savoir plus sur nous</RouterLink
+        >En savoir plus sur moi</RouterLink
       >
     </div>
   </div>
