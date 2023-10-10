3d63e43d5f5823a8b10d1a0b21629e9eac95fd67
aimielbn <aimie.lebon@ifosup.wavre.be>
2023-10-10 11:40:21 +0200

    wip

--- pages/Projets.vue
+++ pages/Projets.vue
@@ -3,10 +3,33 @@
 
   <h2>Maquettes</h2>
 
-  <h2>Visuels</h2>
   <div class="d-flex justify-content-evenly flex-wrap">
     <div class="col-12 col-md-5 col-lg-3 col-xl-2 card mb-4">
-      <img src="/img/visuel1.jpg" class="card-img-top" alt="projet 1" />
+      <img src="/img/maquette1.jpg" class="card-img-top" alt="maquette 1" />
+      <div class="card-body">
+        <h5 class="card-title">"Saveurs des thés"</h5>
+        <p class="card-text">
+          Maquette desktop d'un site qui présenterait ses thés.
+        </p>
+      </div>
+    </div>
+
+    <div class="col-12 col-md-5 col-lg-3 col-xl-2 card mb-4">
+      <img src="/img/maquette2.jpg" class="card-img-top" alt="maquette 2" />
+      <div class="card-body">
+        <h5 class="card-title">"Saveurs des thés"</h5>
+        <p class="card-text">
+          Maquette téléphone d'un site qui présenterait ses thés.
+        </p>
+      </div>
+    </div>
+  </div>
+
+  <h2>Visuels</h2>
+
+  <div class="d-flex justify-content-evenly flex-wrap">
+    <div class="col-12 col-md-5 col-lg-3 col-xl-2 card mb-4">
+      <img src="/img/visuel1.jpg" class="card-img-top" alt="visuel 1" />
       <div class="card-body">
         <h5 class="card-title">"PastaKids"</h5>
         <p class="card-text">
@@ -20,13 +43,13 @@
       <div id="carouselExample" class="carousel slide">
         <div class="carousel-inner">
           <div class="carousel-item active">
-            <img src="/img/visuel2.jpg" class="d-block w-100" alt="..." />
+            <img src="/img/visuel2.jpg" class="d-block w-100" alt="visuel 2" />
           </div>
           <div class="carousel-item">
-            <img src="/img/visuel6.jpg" class="d-block w-100" alt="..." />
+            <img src="/img/visuel6.jpg" class="d-block w-100" alt="visuel 2" />
           </div>
           <div class="carousel-item">
-            <img src="/img/visuel7.jpg" class="d-block w-100" alt="..." />
+            <img src="/img/visuel7.jpg" class="d-block w-100" alt="visuel 2" />
           </div>
         </div>
         <button
@@ -58,7 +81,7 @@
     </div>
 
     <div class="col-12 col-md-5 col-lg-3 col-xl-2 card mb-4">
-      <img src="/img/visuel3.jpg" class="card-img-top" alt="projet 1" />
+      <img src="/img/visuel3.jpg" class="card-img-top" alt="visuel 3" />
       <div class="card-body">
         <h5 class="card-title">"Bijouterie"</h5>
         <p class="card-text">
@@ -66,12 +89,37 @@
         </p>
       </div>
     </div>
+
+    <div class="col-12 col-md-5 col-lg-3 col-xl-2 card mb-4">
+      <img src="/img/visuel4.jpg" class="card-img-top" alt="visuel 4" />
+      <div class="card-body">
+        <h5 class="card-title">"Armony"</h5>
+        <p class="card-text">
+          Newsletter téléphone pour une entreprise qui créé des ambiances dans
+          des pièces de maisons et qui organise des rangements de sorte à avoir
+          un espace optimal.
+        </p>
+      </div>
+    </div>
+
+    <div class="col-12 col-md-5 col-lg-3 col-xl-2 card mb-4">
+      <img src="/img/visuel5.jpg" class="card-img-top" alt="visuel 5" />
+      <div class="card-body">
+        <h5 class="card-title">"Armony"</h5>
+        <p class="card-text">
+          Newsletter desktop pour une entreprise qui créé des ambiances dans des
+          pièces de maisons et qui organise des rangements de sorte à avoir un
+          espace optimal.
+        </p>
+      </div>
+    </div>
   </div>
 
   <h2>Logos</h2>
+
   <div class="d-flex justify-content-evenly flex-wrap">
     <div class="col-12 col-md-5 col-lg-3 col-xl-2 card mb-4">
-      <img src="/img/logo1.jpg" class="card-img-top" alt="projet 1" />
+      <img src="/img/logo1.jpg" class="card-img-top" alt="logo 1" />
       <div class="card-body">
         <h5 class="card-title">"Les ChaChoux"</h5>
         <p class="card-text">Logo pour un bar à chats.</p>
@@ -79,7 +127,7 @@
     </div>
 
     <div class="col-12 col-md-5 col-lg-3 col-xl-2 card mb-4">
-      <img src="/img/logo2.jpg" class="card-img-top" alt="projet 1" />
+      <img src="/img/logo2.jpg" class="card-img-top" alt="logo 2" />
       <div class="card-body">
         <h5 class="card-title">"Armony"</h5>
         <p class="card-text">
@@ -87,6 +135,14 @@
           maisons et qui organise des rangements de sorte à avoir un espace
           optimal.
         </p>
+      </div>
+    </div>
+
+    <div class="col-12 col-md-5 col-lg-3 col-xl-2 card mb-4">
+      <img src="/img/logo3.jpg" class="card-img-top" alt="logo 3" />
+      <div class="card-body">
+        <h5 class="card-title">"Barbie"</h5>
+        <p class="card-text">Logo Barbie existant repensé et modifié.</p>
       </div>
     </div>
   </div>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,9 @@
 <script setup></script>
 
 <template>
-<footer class="footer mt-auto py-3 bg-body-tertiary">
-      <div class="container">
-        <span class="text-body-secondary"
-          >Place sticky footer content here.</span
-        >
-      </div>
-    </footer>
-    </template>
+  <footer class="footer mt-auto py-3 bg-body-tertiary">
+    <div class="container">
+      <span class="text-body-secondary">©2023 Tous droits réservés.</span>
+    </div>
+  </footer>
+</template>
