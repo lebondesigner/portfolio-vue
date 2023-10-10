8a3bb132efafcaacf35aab1e8e533cb70523f402
aimielbn <aimie.lebon@ifosup.wavre.be>
2023-10-10 10:13:03 +0200

    wip

--- pages/Home.vue
+++ pages/Home.vue
@@ -8,26 +8,30 @@ import { RouterLink } from "vue-router";
       <h1 class="display-4">Mon portfolio</h1>
       <p class="lead">Voici mes projets et des infos sur moi !</p>
 
-      <div class="card" style="width: 18rem">
-        <img src="../public/img/logo1.jpg" class="card-img-top" alt="..." />
-        <div class="card-body">
-          <h5 class="card-title">"Les Chachoux"</h5>
-          <p class="card-text">Logo pour un bar à chats</p>
+      <div class="d-flex justify-content-evenly flex-wrap">
+        <div class="col-12 col-md-5 col-lg-3 col-xl-2 card mb-4">
+          <img src="/img/logo1.jpg" class="card-img-top" alt="projet 1" />
+          <div class="card-body">
+            <h5 class="card-title">"Les ChaChoux"</h5>
+            <p class="card-text">Logo pour un bar à chats.</p>
+          </div>
         </div>
 
-        <img src="../public/img/visuel5.jpg" class="card-img-top" alt="..." />
-        <div class="card-body">
-          <h5 class="card-title">"Newsletter de juin"</h5>
-          <p class="card-text">
-            Newsletter pour une entreprise qui créé des ambiances dans des
-            pièces de maisons et fait des rangements pratiques afin d'optimiser
-            l'espace.
-          </p>
+        <div class="col-12 col-md-5 col-lg-3 col-xl-2 card mb-4">
+          <img src="/img/visuel5.jpg" class="card-img-top" alt="projet 1" />
+          <div class="card-body">
+            <h5 class="card-title">"Newsletter"</h5>
+            <p class="card-text">
+              Newsletter pour une entreprise qui créé des ambiances dans des
+              pièces de maisons et qui organise des rangements de sorte à avoir
+              un espace optimal.
+            </p>
+          </div>
         </div>
       </div>
 
       <RouterLink to="/projets" class="btn btn-primary btn-lg"
-        >Voir mes projets</RouterLink
+        >Voir plus de projets</RouterLink
       >
     </div>
   </div>
--- pages/Projets.vue
+++ pages/Projets.vue
@@ -4,8 +4,91 @@
   <h2>Maquettes</h2>
 
   <h2>Visuels</h2>
+  <div class="d-flex justify-content-evenly flex-wrap">
+    <div class="col-12 col-md-5 col-lg-3 col-xl-2 card mb-4">
+      <img src="/img/visuel1.jpg" class="card-img-top" alt="projet 1" />
+      <div class="card-body">
+        <h5 class="card-title">"PastaKids"</h5>
+        <p class="card-text">
+          Visuel de pub sur Instagram pour une entreprise organisant des stages
+          pour des ateliers cuisine avec vos enfants.
+        </p>
+      </div>
+    </div>
+
+    <div class="col-12 col-md-5 col-lg-3 col-xl-2 card mb-4">
+      <div id="carouselExample" class="carousel slide">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="/img/visuel2.jpg" class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img src="/img/visuel6.jpg" class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img src="/img/visuel7.jpg" class="d-block w-100" alt="..." />
+          </div>
+        </div>
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExample"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExample"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">"Space Coaster"</h5>
+        <p class="card-text">
+          Visuel de bannières défilantes pour le site web d'un parc
+          d'attraction.
+        </p>
+      </div>
+    </div>
+
+    <div class="col-12 col-md-5 col-lg-3 col-xl-2 card mb-4">
+      <img src="/img/visuel3.jpg" class="card-img-top" alt="projet 1" />
+      <div class="card-body">
+        <h5 class="card-title">"Bijouterie"</h5>
+        <p class="card-text">
+          Icônes que l'on pourrait trouver sur le site d'une bijouterie.
+        </p>
+      </div>
+    </div>
+  </div>
 
   <h2>Logos</h2>
+  <div class="d-flex justify-content-evenly flex-wrap">
+    <div class="col-12 col-md-5 col-lg-3 col-xl-2 card mb-4">
+      <img src="/img/logo1.jpg" class="card-img-top" alt="projet 1" />
+      <div class="card-body">
+        <h5 class="card-title">"Les ChaChoux"</h5>
+        <p class="card-text">Logo pour un bar à chats.</p>
+      </div>
+    </div>
 
+    <div class="col-12 col-md-5 col-lg-3 col-xl-2 card mb-4">
+      <img src="/img/logo2.jpg" class="card-img-top" alt="projet 1" />
+      <div class="card-body">
+        <h5 class="card-title">"Armony"</h5>
+        <p class="card-text">
+          Logo pour une entreprise qui créé des ambiances dans des pièces de
+          maisons et qui organise des rangements de sorte à avoir un espace
+          optimal.
+        </p>
+      </div>
+    </div>
+  </div>
   <h2>Sites</h2>
 </template>
