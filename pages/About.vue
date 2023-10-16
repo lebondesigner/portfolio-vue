e936420037b8ab861d0a6adb40fd674f602bc2f9
aimielbn <aimie.lebon@ifosup.wavre.be>
2023-10-16 09:22:17 +0200

    wip

--- pages/About.vue
+++ pages/About.vue
@@ -1,11 +1,16 @@
 <template>
   <h2>À propos de moi</h2>
+  <p>Je m'appelle Aimie Lebon et j'ai 20 ans.</p>
   <p>
-    Je m'appelle Aimie Lebon et j'ai 20 ans. 
+    Je suis en deuxième année de Webdesign. J'aime principalement faire des
+    logos en utilisant Illustrator, des maquettes sur Adobe XD, des visuels.
   </p>
   <p>
-    Je suis en deuxième année de Webdesign ! 
-    J'aime principalement faire des logos en utilisant Illustrator,
-    des maquettes sur Adobe XD, des visuels.
+    Je sais également utiliser Photoshop. J'apprends sur d'autres logiciels de
+    la suite Adobe comme Indesign, Premiere Pro et After Effect.
+  </p>
+  <p>
+    Je sais faire des sites en utilisant de l'HTML et du CSS. J'ai aussi appris
+    à utiliser du PHP et du JavaScript.
   </p>
 </template>
